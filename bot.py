d4e2270269bcc0d0fc597e8a379908fb926fff4b
Jaden <JadenJih@gmail.com>
2026-06-17 19:48:44 -0400

    Added the cmd to check ping and to check info

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ui import Button, View
 
 intents=discord.Intents.default()
 intents.message_content=True
@@ -16,4 +17,52 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send(f'{ctx.author.name} hello!')
 
-  
+@bot.command()
+async def ping(ctx):
+    latency=round(bot.latency*1000)
+    await ctx.send(f'{latency}ms')
+
+@bot.command()
+async def info(ctx):
+
+    embed=discord.Embed(
+        title=f"{ctx.author.name} Your info",
+        description="making discord bot",
+        color=0x00ff00 
+    )
+
+    embed.add_field(name="creation date",value=ctx.author.created_at.strftime("%yyear%mmonth%dday"),inline=False)
+
+    embed.set_thumbnail(url=ctx.author.avatar.url if ctx.author.avatar else ctx.author.default_avatar.url)
+
+    await ctx.send(embed=embed)
+
+
+    #Fruit gambling game which requires currency and a selling mechanic
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.run('MTUxNjkyNzkzNDgzMTY1NzE1Mg.GyaDAK.S_p_aHoUavMvuJyQlCsVjCuO_dJWkC06rSg9T0')
