9c9ea9a4e61c85a56c6a461c6155dcf54785135d
Jaden <JadenJih@gmail.com>
2026-06-17 19:11:47 -0400

    Bot can now do simple hello command

--- bot.py
+++ bot.py
@@ -1,3 +1,19 @@
 import discord
+from discord.ext import commands
 
-print("Hello")
+intents=discord.Intents.default()
+intents.message_content=True
+bot=commands.Bot(command_prefix='/',intents=intents)
+
+@bot.event
+async def on_ready():
+    print('==============================')
+    print(f'login sucessful: {bot.user.name}')
+    print('bot sucessfuly launched')
+    print('==============================')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'{ctx.author.name} hello!')
+
+  
